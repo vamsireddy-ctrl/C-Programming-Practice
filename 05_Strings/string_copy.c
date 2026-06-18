099dff1e4fde7862b9e5aa5d96b1e59202ad4ad4
vamsireddy-ctrl <madibakavamsi@gmail.com>
2026-06-18 08:17:29 +0530

    Added String Copy Program

---
+++ 05_Strings/string_copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    while(str1[i] != '\0') {
+        str2[i] = str1[i];
+        i++;
+    }
+
+    str2[i] = '\0';
+
+    printf("Copied String: %s", str2);
+
+    return 0;
+}
